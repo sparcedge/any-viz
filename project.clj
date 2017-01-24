63a2acb7f928839b17732fbc786861cf3d500565
Bob Williams <bobwilliams.ii@gmail.com>
2017-01-24 11:26:50 -0500

    dependency updates

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.8"]
+                 [http-kit "2.2.0"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.4"]
+                 [hiccup "1.0.5"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-devel "1.1.8"]
-                 [clj-http "0.7.4"]
-                 [cheshire "5.2.0"]
+                 [clj-http "2.3.0"]
+                 [cheshire "5.7.0"]
                  [tentacles "0.3.0"]
-                 [clj-time "0.5.1"]])
+                 [clj-time "0.13.0"]])
